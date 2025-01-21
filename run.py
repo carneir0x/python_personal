90a57323d2e24364d8ca46bb380997566856a0d5
Raphael Carneiro <raphael_carneiro@hotmail.com>
2025-01-21 19:12:14 +0000

    Tela de início!

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 def initial_screen():
-    print('Testing, hello word.')
+    welcome_greeting = '\nWelcome to the Personal Trainer Tool\n'
+    print(welcome_greeting.upper())
+    print('Made for user by Personal Trainer Professional\n')
+    print('To use this app, hit enter after each choice.\n')
 
 initial_screen()
