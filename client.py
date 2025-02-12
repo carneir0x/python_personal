7223808fe3317b67083cafc879aae854eab6d6f5
Raphael Carneiro <raphael_carneiro@hotmail.com>
2025-02-12 17:38:55 +0000

    continuação da implementação da função process details/

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+#classe usada para representar um Cliente
 class Client:
     def __init__(self, name, last_name, gender, height, weight, age, act_level):
         self.name = name
@@ -8,10 +9,12 @@ class Client:
         self.age = age
         self.act_level = act_level
 
+    #descreve a instância do novo Cliente
     def description(self):
-        print(f"Name: {self.name}), Last Name: {self.last_name}, Gender: {self.gender,}, Height: {self.height}, Weight: {self.weight}, Age: {self.age}, Activite Level: {self.act_level}")
+        print(f"Name: {self.name}, Last Name: {self.last_name}, Gender: {self.gender}, Height: {self.height}, Weight: {self.weight}, Age: {self.age}, Activite Level: {self.act_level}")
         return "\n\nClient succesfully created!\n"
 
+#função para obter a entrada do nome e validar
 def get_name():
     while True:
         name = input('Client name? \n\n').lower()
@@ -19,6 +22,7 @@ def get_name():
             return name
         print('\nInvalid input! All characters should be alphabet letters')
 
+#função para obter o sobrenome e validar
 def get_lastname():
     while True:
         lastname = input("Client last name? \n\n").lower()
@@ -26,6 +30,7 @@ def get_lastname():
             return lastname
         print('\nInvalid input! All characters should be alphabet letters')
 
+#função para obter o gênero e validar
 def get_gender():
     while True:
         gender = input("Client gender? Hit F for female or M for male.\n")
@@ -36,6 +41,7 @@ def get_gender():
         else: 
             print("Invalid input!")
 
+#função para obter a altura e validar
 def get_height():
     while True:
         height = input("Client height (CM)? It MUST be in Centimeters!!\n\n")
@@ -43,12 +49,14 @@ def get_height():
             return int(height)
         print("\nInvalid input! All characters should be numbers (1-10)")
 
+#função para obter o peso e validar
 def get_weight():
     while True:
         weight = input("Client weight (kg) ? It MUST be in Kilogram!!\n\n")
         if weight.isnumeric():
             return int(weight)
 
+#função para obter a idade e validar
 def get_age():
     while True:
         age = input("Client age? \n\n")
@@ -56,8 +64,29 @@ def get_age():
             return int(age)
         print("\nInvalid input! All characters should be numbers (1-10)")
 
+#função para obter a entrada no nível de atividade e validar
 def get_activite_level():
-    return int(1)
+    print("Client Activite Level? Digit one of the following options:\n")
+    print("'1' for sedentary (little or no exercise)")
+    print("'2' for lightly active (light exercise 1-3 days/week)")
+    print("'3' for moderately active (moderate exercise 3-5 days/week")
+    print("'4' for very active (hard exercise/sports 6-7 days a week)")
+    print("'5' for extra active (very hard exercise/sports & physical job)")
+    while True:
+        activite = input()
+
+        if activite == "1":
+            return float(activite)
+        elif activite == "2":
+            return float(activite)
+        elif activite == "3":
+            return float(activite)
+        elif activite == "4":
+            return float(activite)
+        elif activite == "5":
+            return float(activite)
+        else:
+            print("Invalid input!")
 
 
     
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import string
 
+from random import choice
 from client import Client
 from client import get_name, get_lastname
 from client import get_gender, get_height
@@ -39,12 +40,12 @@ def new_client():
 
 def process_client_details():
     print("Let's get client personal data...\n")
-    new_client = Client(get_name(), get_lastname(), get_gender(), get_height(), get_weight(), get_age(), get_activite_level)
+    new_client = Client(get_name(), get_lastname(), get_gender(), get_height(), get_weight(), get_age(), get_activite_level())
     print("\n\nProcessing New Client data...\n\n ")
     print(new_client.description())
    # next()
 
-    #criando variáveis separadas para facilitar interação 
+    #inserindo valores informados do Cliente em variáveis separadas para facilitar interação
     name = new_client.name
     lastname = new_client.last_name
     gender = new_client.gender
@@ -53,8 +54,103 @@ def process_client_details():
     age = new_client.age
     act_level = float(new_client.act_level)
 
+    #chama a função create_id e informa ao novo Cliente
+    id = int(create_id())
+
+    #calcule o IMC chamando sua função e passe para o novo cliente.
+    bmi = check_bmi(name, weight, height)
+    #next()   
+
+    #calcule o BMR chamando sua função e passe para o novo cliente.
+    bmr = check_bmr(name, gender, weight, height, age)
+    #next()
+
+    #Calcule a dieta chamando sua função e exiba o resultado final
+    diet_process = create_diet(name, bmr, act_level)
+    loss = int(diet_process * 0.85) #menos 15% KCAL por dia para perder peso
+    mantain = int(diet_process)
+    gain = int(diet_process * 1.15) #mais 15% KCAL por dia para ganhar peso 
+
+    print(f"For WEIGHT LOSS it's recommended: {loss} KCAL daily.")
+    print(f"For MANTAIN WEIGHT it's recommended: {mantain} KCAL daily.")
+    print(f"For WEIGHT GAIN it's recommended: {gain} KCAL daily.")
 
     
 
+#função para criar um id único para cada cliente com 6 digitos
+def create_id():
+    chars = string.digits
+    random = ''.join(choice(chars) for _ in range(6))
+    id = random
+    return id
 
+#função para calcular o IMC do cliente
+def check_bmi(name, weight, height):
+    print("-------------------------\n")
+    print("Let's calculate their (BMI)")
+    print("BMI (body mass index) is a measure of whether")
+    print("you're a healthy weight for your height\n")
+
+    #Calcule o IMC e mostre para o usuário
+    bmi_check = weight / (height/100)**2
+    bmi = round(bmi_check,1)
+    print(f"{name} BMI is: {bmi}\n")
+
+    #Condição para verificar se o cliente está saudável ou não pelo IMC
+    if bmi <= 18.4:
+        print(f"{name} is underweight.\n")
+    elif bmi <= 24.9:
+        print(f"{name} is healthy.\n")
+    elif bmi <= 29.9:
+        print(f"{name} is over weight.\n")
+    elif bmi <= 34.9:
+        print(f"{name} is severely over weight.\n")
+    elif bmi <= 39.9:
+        print(f"{name} is obese.\n")
+    else:
+        print(f"{name} is severely obese.\n")
+    return bmi
+
+#função para calcular o BMR do cliente (Taxa metabólica basal)
+def check_bmr(name, gender, weight, height, age):
+    print("Let's calculate their (BMR)")
+    print("BMR (Basal metabolic rate) is the amount of")
+    print("energy expended per day at rest.")
+    print("This is fundamental to decide either if it is needed to consume")
+    print("more or less KCAL per day, depending on the client objective\n")
+
+    if gender == "m":
+        brm = 10 * weight + 6.25 * height - 5 * age + 5
+    else:
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161
+
+    print(f"{name} BMR is: {bmr}\n")
+    return bmr
+
+
+def create_diet (name, bmr, act_level):
+    #Função para calcular o consumo real de KCAL do cliente, dependendo do nível de atividade
+    print("-------------------------\n")
+    print("Let's create a daily KCAL diet")   
+
+    if act_level == 1:
+        real_bmr = bmr * 1.2
+    elif act_level == 2:
+        real_bmr = bmr * 1.375
+    elif act_level == 3:
+        real_bmr = bmr * 1.55
+    elif act_level == 4:
+        real_bmr = bmr * 1.725
+    else:
+        real_bmr = bmr * 1.9
+
+    print (f"Prossesing {name} diet...\n\n")
+    return real_bmr
+
+
+
+
+
+
+    
 initial_screen()
